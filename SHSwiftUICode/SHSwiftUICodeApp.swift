3bbaa6e63476cfe04eeb3152c9fa595be0edfdbd
DJSeokHo <djseokho@vip.qq.com>
2022-01-15 14:40:08 +0900

    Initial Commit

---
+++ SHSwiftUICode/SHSwiftUICodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SHSwiftUICodeApp.swift
+//  SHSwiftUICode
+//
+//  Created by Seok Ho on 2022/01/15.
+//
+
+import SwiftUI
+
+@main
+struct SHSwiftUICodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

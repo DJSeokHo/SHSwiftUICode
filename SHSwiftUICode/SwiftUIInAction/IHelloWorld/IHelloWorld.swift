21563a8a772dfe1aefb2b9ba15647c027e01405c
CodingWithCat <djseokho@vip.qq.com>
2022-04-06 12:53:59 +0900

    Seok Ho

--- SHSwiftUICode/SwiftUIInAction/IHelloWorld/IHelloWorld.swift
+++ SHSwiftUICode/SwiftUIInAction/IHelloWorld/IHelloWorld.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct IHelloWorld: View {
     var body: some View {
@@ -20,19 +21,52 @@ private struct ContentView: View {
     
     var body: some View {
         
-        Button(action: {
-            ILog.debug(tag: #file, content: "Hello World!")
-        }, label: {
-          
-            Text("Hello World")
-                .fontWeight(.bold)
-                .font(.system(.title, design: .rounded))
+        VStack {
             
-        })
-        .padding()
-        .foregroundColor(.white)
-        .background(Color.purple)
-        .cornerRadius(20)
+            Button(action: {
+             
+                speak(text: "Happy Programming")
+                
+            }, label: {
+              
+                Text("Happy Programming")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .rounded))
+                
+            })
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.yellow)
+            .cornerRadius(20)
+            
+            Button(action: {
+                
+               speak(text: "Hello World")
+                
+            }, label: {
+              
+                Text("Hello World")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .rounded))
+                
+            })
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.purple)
+            .cornerRadius(20)
+            
+        }
+        
+    }
+    
+    func speak(text: String) {
+        
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+        
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
+        
     }
 }
 

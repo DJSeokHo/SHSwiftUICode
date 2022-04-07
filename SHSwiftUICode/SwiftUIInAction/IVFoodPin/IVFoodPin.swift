777aed92784a5551d194704ce1d199fc382f1356
CodingWithCat <djseokho@vip.qq.com>
2022-04-07 13:58:22 +0900

    Seok Ho

--- SHSwiftUICode/SwiftUIInAction/IVFoodPin/IVFoodPin.swift
+++ SHSwiftUICode/SwiftUIInAction/IVFoodPin/IVFoodPin.swift
@@ -10,37 +10,40 @@ import SwiftUI
 @available(iOS 15.0, *)
 struct RestaurantListView: View {
     
-    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
-    
-    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
-
-    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
-    
-    
     @State
-    var restaurantIsFavorites = Array(repeating: false, count: 21)
-    
+    var restaurants = [
+        Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "Hong Kong", image: "Cafe Deadend", isFavorite: false),
+        Restaurant(name: "Homei", type: "Cafe", location: "Hong Kong", image: "Homei", isFavorite: false),
+        Restaurant(name: "Teakha", type: "Tea House", location: "Hong Kong", image: "Teakha", isFavorite: false),
+        Restaurant(name: "Cafe loisl", type: "Austrian / Causual Drink", location: "Hong Kong", image: "Cafe Loisl", isFavorite: false),
+        Restaurant(name: "Petite Oyster", type: "French", location: "Hong Kong", image: "Petite Oyster", isFavorite: false),
+        Restaurant(name: "For Kee Restaurant", type: "Bakery", location: "Hong Kong", image: "For Kee Restaurant", isFavorite: false),
+        Restaurant(name: "Po's Atelier", type: "Bakery", location: "Hong Kong", image: "Po's Atelier", isFavorite: false),
+        Restaurant(name: "Bourke Street Bakery", type: "Chocolate", location: "Sydney", image: "Bourke Street Bakery", isFavorite: false),
+        Restaurant(name: "Haigh's Chocolate", type: "Cafe", location: "Sydney", image: "Haigh's Chocolate", isFavorite: false),
+        Restaurant(name: "Palomino Espresso", type: "American / Seafood", location: "Sydney", image: "Palomino Espresso", isFavorite: false),
+        Restaurant(name: "Upstate", type: "American", location: "New York", image: "Upstate", isFavorite: false),
+        Restaurant(name: "Traif", type: "American", location: "New York", image: "Traif", isFavorite: false),
+        Restaurant(name: "Graham Avenue Meats", type: "Breakfast & Brunch", location: "New York", image: "Graham Avenue Meats And Deli", isFavorite: false),
+        Restaurant(name: "Waffle & Wolf", type: "Coffee & Tea", location: "New York", image: "Waffle & Wolf", isFavorite: false),
+        Restaurant(name: "Five Leaves", type: "Coffee & Tea", location: "New York", image: "Five Leaves", isFavorite: false),
+        Restaurant(name: "Cafe Lore", type: "Latin American", location: "New York", image: "Cafe Lore", isFavorite: false),
+        Restaurant(name: "Confessional", type: "Spanish", location: "New York", image: "Confessional", isFavorite: false),
+        Restaurant(name: "Barrafina", type: "Spanish", location: "London", image: "Barrafina", isFavorite: false),
+        Restaurant(name: "Donostia", type: "Spanish", location: "London", image: "Donostia", isFavorite: false),
+        Restaurant(name: "Royal Oak", type: "British", location: "London", image: "Royal Oak", isFavorite: false),
+        Restaurant(name: "CASK Pub and Kitchen", type: "Thai", location: "London", image: "CASK Pub and Kitchen", isFavorite: false)
+    ]
+  
     var body: some View {
        
         List {
             
-            ForEach(restaurantNames.indices, id: \.self) { index in
+            ForEach(restaurants.indices, id: \.self) { index in
+              
+//                RestaurantListItemView(restaurant: $restaurants[index])
                 
-//                RestaurantListItemView(
-//                    imageName: restaurantNames[index],
-//                    name: restaurantNames[index],
-//                    type: restaurantTypes[index],
-//                    location: restaurantLocations[index],
-//                    isFavorite: $restaurantIsFavorites[index]
-//                )
-                
-                RestaurantListItemFullImageView(
-                    imageName: restaurantNames[index],
-                    name: restaurantNames[index],
-                    type: restaurantTypes[index],
-                    location: restaurantLocations[index],
-                    isFavorite: $restaurantIsFavorites[index]
-                )
+                RestaurantListItemFullImageView(restaurant: $restaurants[index])
                 
             }
             .listRowSeparator(.hidden)
@@ -59,18 +62,14 @@ private struct RestaurantListItemView: View {
     @State
     private var showError = false
     
-    var imageName: String
-    var name: String
-    var type: String
-    var location: String
     @Binding
-    var isFavorite: Bool
+    var restaurant: Restaurant
     
     var body: some View {
         
         HStack(alignment: .top, spacing: 20) {
             
-            Image(imageName)
+            Image(restaurant.image)
                 .resizable()
                 .frame(width: 120, height: 120)
 //                .cornerRadius(20) // 这个也行
@@ -78,24 +77,24 @@ private struct RestaurantListItemView: View {
             
             VStack(alignment: .leading) {
                 
-                Text(name)
+                Text(restaurant.name)
                     .font(.system(.title2, design: .rounded))
                 
-                Text(type)
+                Text(restaurant.type)
                     .font(.system(.body, design: .rounded))
                 
-                Text(location)
+                Text(restaurant.location)
                     .font(.system(.subheadline, design: .rounded))
                     .foregroundColor(.gray)
             }
             
-            if isFavorite {
+            if restaurant.isFavorite {
                 Spacer()
                 
                 Image(systemName: "heart.fill")
                     .foregroundColor(.yellow)
                     .onTapGesture {
-                        isFavorite.toggle()
+                        restaurant.isFavorite.toggle()
                     }
             }
            
@@ -109,8 +108,8 @@ private struct RestaurantListItemView: View {
                     .default(Text("Reserve a table")) {
                         showError.toggle()
                     },
-                    .default(Text(isFavorite ? "Remove from favorites" : "Mark as favorite")) {
-                        isFavorite.toggle()
+                    .default(Text(restaurant.isFavorite ? "Remove from favorites" : "Mark as favorite")) {
+                        restaurant.isFavorite.toggle()
                     },
                     .cancel()
                 ]
@@ -134,18 +133,14 @@ private struct RestaurantListItemFullImageView: View {
     @State
     private var showError = false
     
-    var imageName: String
-    var name: String
-    var type: String
-    var location: String
     @Binding
-    var isFavorite: Bool
+    var restaurant: Restaurant
     
     var body: some View {
         
         VStack(alignment: .leading, spacing: 10) {
             
-            Image(imageName)
+            Image(restaurant.image)
                 .resizable()
                 .scaledToFill()
                 .frame(height: 200)
@@ -156,24 +151,24 @@ private struct RestaurantListItemFullImageView: View {
                 
                 VStack(alignment: .leading) {
                     
-                    Text(name)
+                    Text(restaurant.name)
                         .font(.system(.title2, design: .rounded))
                     
-                    Text(type)
+                    Text(restaurant.type)
                         .font(.system(.body, design: .rounded))
                     
-                    Text(location)
+                    Text(restaurant.location)
                         .font(.system(.subheadline, design: .rounded))
                         .foregroundColor(.gray)
                 }
                 
-                if isFavorite {
+                if restaurant.isFavorite {
                     Spacer()
                     
                     Image(systemName: "heart.fill")
                         .foregroundColor(.yellow)
                         .onTapGesture {
-                            isFavorite.toggle()
+                            restaurant.isFavorite.toggle()
                         }
                 }
             }
@@ -188,8 +183,8 @@ private struct RestaurantListItemFullImageView: View {
                     .default(Text("Reserve a table")) {
                         showError.toggle()
                     },
-                    .default(Text(isFavorite ? "Remove from favorites" : "Mark as favorite")) {
-                        isFavorite.toggle()
+                    .default(Text(restaurant.isFavorite ? "Remove from favorites" : "Mark as favorite")) {
+                        restaurant.isFavorite.toggle()
                     },
                     .cancel()
                 ]
@@ -215,11 +210,20 @@ struct RestaurantListView_Previews: PreviewProvider {
             
             RestaurantListView()
                 
-            RestaurantListItemView(imageName: "Cafe Deadend", name: "Cafe Deadend", type: "Cafe", location: "Hong Kong", isFavorite: .constant(true))
-                .previewLayout(.sizeThatFits) // 我們不以全尺寸的模擬器預覽列佈局，我只想要將預覽畫面顯示在一個容器中
+            RestaurantListItemView(
+                restaurant: .constant(
+                    Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "Hong Kong", image: "Cafe Deadend", isFavorite: true)
+                )
+            )
+            .previewLayout(.sizeThatFits) // 我們不以全尺寸的模擬器預覽列佈局，我只想要將預覽畫面顯示在一個容器中
 
-            RestaurantListItemFullImageView(imageName: "Cafe Deadend", name: "Cafe Deadend", type: "Cafe", location: "Hong Kong", isFavorite: .constant(true))
-                .previewLayout(.sizeThatFits) // 我們不以全尺寸的模擬器預覽列佈局，我只想要將預覽畫面顯示在一個容器中
+            RestaurantListItemFullImageView(
+                restaurant: .constant(
+                    Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "Hong Kong", image: "Cafe Deadend", isFavorite: true)
+                )
+            )
+            .previewLayout(.sizeThatFits) // 我們不以全尺寸的模擬器預覽列佈局，我只想要將預覽畫面顯示在一個容器中
+            
         }
         .preferredColorScheme(.dark)
     }
---
+++ SHSwiftUICode/SwiftUIInAction/IVFoodPin/Model/Restaurant.swift
@@ -0,0 +1,18 @@
+//
+//  Restaurant.swift
+//  SHSwiftUICode
+//
+//  Created by 浩 SHI on 2022/04/07.
+//
+
+import Foundation
+
+struct Restaurant {
+    
+    var name: String = ""
+    var type: String = ""
+    var location: String = ""
+    var image: String = ""
+    var isFavorite: Bool = false
+
+}

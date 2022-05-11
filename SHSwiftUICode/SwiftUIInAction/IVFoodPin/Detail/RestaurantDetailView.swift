9f5f308c61aa8fa183ae141481ee42af5434611e
DJSeokHo <djseokho@vip.qq.com>
2022-05-11 18:24:13 +0900

    Seok Ho

--- SHSwiftUICode/SwiftUIInAction/IVFoodPin/Detail/RestaurantDetailView.swift
+++ SHSwiftUICode/SwiftUIInAction/IVFoodPin/Detail/RestaurantDetailView.swift
@@ -17,48 +17,65 @@ struct RestaurantDetailView: View {
     
     var body: some View {
         
-        ZStack(alignment: .top) {
-                
-            backgroundImage
+        ScrollView {
             
-            info
+            VStack(alignment: .leading) {
+                
+                image
+                info
+                addressAndPhone
+                
+            }
             
         }
-       
+        .navigationBarBackButtonHidden(true)
+        .toolbar(content: {
+            
+            ToolbarItem(placement: .navigationBarLeading, content: {
+                
+                Button(action: {
+                    dismiss()
+                }, label: {
+                    Text("\(Image(systemName: "chevron.left")) \(restaurant.name)")
+                })
+                
+            })
+            
+        })
+        .ignoresSafeArea()
+
     }
     
-    var backgroundImage: some View {
+    var image: some View {
         
         Image(restaurant.image)
             .resizable()
             .scaledToFill()
             .frame(minWidth: 0, maxWidth: .infinity)
-            .ignoresSafeArea()
+            .frame(height: 445)
+            .overlay(content: {
+                
+                heart
+                headerInfo
+                
+            })
         
     }
     
-    var info: some View {
+    var heart: some View {
         
-        Color.black
-            .frame(height: 100)
-            .opacity(0.8)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
-            .padding()
-            .overlay(content: {
-                
-                VStack(spacing: 5) {
-                    
-                    Text(restaurant.name)
-                    
-                    Text(restaurant.type)
-                    
-                    Text(restaurant.location)
-                    
-                }
-                .padding(.horizontal, 40)
-                .font(.system(size: 13, weight: .medium, design: .rounded))
+        VStack {
+            
+            Image(systemName: "heart")
+            
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topTrailing)
+                .padding()
+                .font(.system(size: 30))
                 .foregroundColor(.white)
-                // MARK: navigationBarBackButtonHidden  修飾器是作為隱藏原始返回按鈕用。然後我們可以使用 toolbar  修飾器來建立自己版本的返回按鈕。
+                .padding(.top, 40)
+
+        }
+        // MARK: navigationBarBackButtonHidden  修飾器是作為隱藏原始返回按鈕用。然後我們可以使用 toolbar  修飾器來建立自己版本的返回按鈕。
 //                .navigationBarBackButtonHidden(true)
 //                .toolbar {
 //
@@ -74,17 +91,82 @@ struct RestaurantDetailView: View {
 //
 //                    }
 //                }
-                
-            })
         
     }
     
+    var headerInfo: some View {
+        
+        VStack(alignment: .leading, spacing: 5, content: {
+            
+            Text(restaurant.name)
+                .font(.custom("Nunito-Regular", size: 35, relativeTo: .largeTitle))
+                .bold()
+            
+            Text(restaurant.type)
+                .font(.system(.headline, design: .rounded))
+                .padding(.all, 5)
+                .background(Color.black)
+            
+        })
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .bottomLeading)
+        .foregroundColor(.white)
+        .padding()
+        
+    }
+    
+    var info: some View {
+        
+        Text(restaurant.description)
+            .padding()
+        
+    }
+    
+    var addressAndPhone: some View {
+        
+        HStack(alignment: .top, content: {
+            
+            VStack(alignment: .leading, content: {
+                
+                Text("ADDRESS")
+                    .font(.system(.headline, design: .rounded))
+                Spacer()
+                    .frame(height: 10)
+                Text(restaurant.location)
+                
+            })
+            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            
+            // MARK: 如果要讓兩個視圖寬度一樣，則將HStack 視圖內的每一個 VStack 視圖的框的寬度設定為 .infinity
+            
+            VStack(alignment: .leading, content: {
+                
+                Text("PHONE")
+                    .font(.system(.headline, design: .rounded))
+                Spacer()
+                    .frame(height: 10)
+                Text(restaurant.phone)
+                
+            })
+            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            
+        })
+        .padding(.horizontal)
+        
+    }
 }
 
 @available(iOS 15.0, *)
 struct RestaurantDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        RestaurantDetailView(restaurant: Restaurant(name: "Cafe Deadend", type: "Cafe", location: "Hong Kong", phone: "", description: "", image: "cafedeadend", isFavorite: true))
-            .previewDevice("iPhone 12")
+        
+        NavigationView {
+            RestaurantDetailView(restaurant: Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "G/F, 72 Po Hing Fong, Sheung Wan, Hong Kong", phone: "232-923423", description: "Searching for great breakfast eateries and coffee? This place is for you. We open at 6:30 every morning, and close at 9 PM. We offer espresso and espresso based drink, such as capuccino, cafe latte, piccolo and many more. Come over and enjoy a great meal.", image: "cafedeadend", isFavorite: false))
+                .previewDevice("iPhone 12")
+        }
+        .accentColor(.white)
+        .previewDevice("iPhone 12")
+      
+//        RestaurantDetailView(restaurant: Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location: "G/F, 72 Po Hing Fong, Sheung Wan, Hong Kong", phone: "232-923423", description: "Searching for great breakfast eateries and coffee? This place is for you. We open at 6:30 every morning, and close at 9 PM. We offer espresso and espresso based drink, such as capuccino, cafe latte, piccolo and many more. Come over and enjoy a great meal.", image: "cafedeadend", isFavorite: false))
+//            .previewDevice("iPhone 12")
     }
 }
